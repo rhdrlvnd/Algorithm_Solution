6bf9cf338899d9e0325cb6f359832e1be1b82795
Dawoon Lee <rhdrlvnd@gmail.com>
2021-01-17 00:47:19 +0900

    BOJ 1026 보물

---
+++ BOJ_1026_보물.java
@@ -0,0 +1,39 @@
+package com.ssafy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1026_보물 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] A = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] B = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			ans += A[i] * B[N - i - 1];
+		}
+		System.out.println(ans);
+	}
+
+}
